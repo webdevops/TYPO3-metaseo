2bcf8b039c7d1382f3551a72c846903287ca5bdb
Markus Blaschke <msb@lightwerk.com>
2014-09-14 17:20:50 +0200

    [TASK] Moved TCA overrides into Configuration/TCA/Overrides/

---
+++ Configuration/TCA/Overrides/pages_language_overlay.php
@@ -0,0 +1,104 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+$tempColumns = array(
+	'tx_metaseo_pagetitle'        => array(
+		'exclude' => 1,
+		'label'   => 'LLL:EXT:metaseo/locallang_db.xml:pages_language_overlay.tx_metaseo_pagetitle',
+		'config'  => array(
+			'type'     => 'input',
+			'size'     => '30',
+			'max'      => '255',
+			'checkbox' => '',
+			'eval'     => 'trim',
+		)
+	),
+	'tx_metaseo_pagetitle_rel'    => array(
+		'exclude' => 1,
+		'label'   => 'LLL:EXT:metaseo/locallang_db.xml:pages_language_overlay.tx_metaseo_pagetitle_rel',
+		'config'  => array(
+			'type'     => 'input',
+			'size'     => '30',
+			'max'      => '255',
+			'checkbox' => '',
+			'eval'     => 'trim',
+		)
+	),
+	'tx_metaseo_pagetitle_prefix' => array(
+		'label'   => 'LLL:EXT:metaseo/locallang_db.xml:pages.tx_metaseo_pagetitle_prefix',
+		'exclude' => 1,
+		'config'  => array(
+			'type'     => 'input',
+			'size'     => '30',
+			'max'      => '255',
+			'checkbox' => '',
+			'eval'     => 'trim',
+		)
+	),
+	'tx_metaseo_pagetitle_suffix' => array(
+		'label'   => 'LLL:EXT:metaseo/locallang_db.xml:pages.tx_metaseo_pagetitle_suffix',
+		'exclude' => 1,
+		'config'  => array(
+			'type'     => 'input',
+			'size'     => '30',
+			'max'      => '255',
+			'checkbox' => '',
+			'eval'     => 'trim',
+		)
+	),
+	'tx_metaseo_canonicalurl'     => array(
+		'label'   => 'LLL:EXT:metaseo/locallang_db.xml:pages.tx_metaseo_canonicalurl',
+		'exclude' => 1,
+		'config'  => array(
+			'type'     => 'input',
+			'size'     => '30',
+			'max'      => '255',
+			'checkbox' => '',
+			'eval'     => 'trim',
+			'wizards'  => Array(
+				'_PADDING' => 2,
+				'link'     => Array(
+					'type'         => 'popup',
+					'title'        => 'Link',
+					'icon'         => 'link_popup.gif',
+					'script'       => 'browse_links.php?mode=wizard&act=url',
+					'params'       => array(
+						'blindLinkOptions' => 'mail',
+					),
+					'JSopenParams' => 'height=300,width=500,status=0,menubar=0,scrollbars=1'
+				),
+			),
+		)
+	),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages_language_overlay', $tempColumns, 1);
+
+// TCA Palettes
+$GLOBALS['TCA']['pages_language_overlay']['palettes']['tx_metaseo_pagetitle'] = array(
+	'showitem'       => 'tx_metaseo_pagetitle,--linebreak--,tx_metaseo_pagetitle_prefix,tx_metaseo_pagetitle_suffix',
+	'canNotCollapse' => 1
+);
+
+$GLOBALS['TCA']['pages_language_overlay']['palettes']['tx_metaseo_crawler'] = array(
+	'showitem'       => 'tx_metaseo_canonicalurl',
+	'canNotCollapse' => 1
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+	'pages_language_overlay',
+	'tx_metaseo_pagetitle_rel',
+	'',
+	'after:title'
+);
+
+// Put it for standard page overlay
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+	'pages_language_overlay',
+	'--div--;LLL:EXT:metaseo/locallang_tca.xml:pages.tab.seo;,--palette--;LLL:EXT:metaseo/locallang_tca.xml:pages.palette.pagetitle;tx_metaseo_pagetitle,--palette--;LLL:EXT:metaseo/locallang_tca.xml:pages.palette.crawler;tx_metaseo_crawler',
+	'',
+	'after:author_email'
+);
+
