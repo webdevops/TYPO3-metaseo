83ffe104205bbe48ffcf2be6099b5195723a4b64
Markus Blaschke <mblaschke82@gmail.com>
2015-05-05 23:32:46 +0200

    Updated TYPO3 version to 7.2.x
    Related #33

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints'                   => array(
         'depends'   => array(
             'php'     => '5.3.0-0.0.0',
-            'typo3'   => '6.2.1-7.1.99',
+            'typo3'   => '6.2.1-7.2.99',
             'extbase' => '6.2.0-6.2.99',
         ),
         'conflicts' => array(),
