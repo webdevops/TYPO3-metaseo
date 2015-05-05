5052369167961a06519f6494a53a4e117e69174e
Markus Blaschke <mblaschke82@gmail.com>
2015-05-05 23:35:32 +0200

    Fixed deps

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,6 @@ $EM_CONF[$_EXTKEY] = array(
         'depends'   => array(
             'php'     => '5.3.0-0.0.0',
             'typo3'   => '6.2.1-7.2.99',
-            'extbase' => '6.2.0-6.2.99',
         ),
         'conflicts' => array(),
         'suggests'  => array(),
