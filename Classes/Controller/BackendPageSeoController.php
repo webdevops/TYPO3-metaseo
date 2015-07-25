eead771404330a640aa50802aad7271590e022aa
Thomas Mayer <thomas.mayer@2bis10.de>
2015-07-25 13:34:59 +0200

    [TASK] don't return result of void method

--- Classes/Controller/BackendPageSeoController.php
+++ Classes/Controller/BackendPageSeoController.php
@@ -52,7 +52,7 @@ class BackendPageSeoController extends AbstractStandardModule
      */
     public function mainAction()
     {
-        return $this->handleSubAction('metadata');
+        $this->handleSubAction('metadata');
     }
 
     /**
