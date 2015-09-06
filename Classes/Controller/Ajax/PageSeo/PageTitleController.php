webdevops/TYPO3-metaseo

commit a78f73045857f3a133b0c525b33f54cb2663fdff

[TASK] OOP: Refactor Ajax controllers
* OOP: Use Subclasses for PageSeo Ajax Controllers #160
* ExceptionHandler in Ajax Methods #161
* Use constants for Ajax controller names #159
* Use reflections to determine Ajax actions #155
* Unregister advanced Ajax controller #158

Fixes #160
Fixes #159
Fixes #158
Fixes #161
Fixes #155

Classes/Controller/Ajax/PageSeo/PageTitleController.php
@@ -1,0 +1,48 @@
+<?php
+
+/*
+ *  Copyright notice
+ *
+ *  (c) 2015 Markus Blaschke <typo3@markus-blaschke.de> (metaseo)
+ *  (c) 2013 Markus Blaschke (TEQneers GmbH & Co. KG) <blaschke@teqneers.de> (tq_seo)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+namespace Metaseo\Metaseo\Controller\Ajax\PageSeo;
+
+use Metaseo\Metaseo\Controller\Ajax\AbstractPageSeoController;
+use Metaseo\Metaseo\Controller\Ajax\PageSeoInterface;
+
+class PageTitleController extends AbstractPageSeoController implements PageSeoInterface
+{
+    const LIST_TYPE = 'pagetitle';
+
+    /**
+     * @inheritDoc
+     */
+    protected function initFieldList()
+    {
+        $this->fieldList = array(
+            'tx_metaseo_pagetitle',
+            'tx_metaseo_pagetitle_rel',
+            'tx_metaseo_pagetitle_prefix',
+            'tx_metaseo_pagetitle_suffix',
+        );
+    }
+}

Classes/Controller/Ajax/PageSeo/PageTitleSimController.php
@@ -1,0 +1,157 @@
+<?php
+
+/*
+ *  Copyright notice
+ *
+ *  (c) 2015 Markus Blaschke <typo3@markus-blaschke.de> (metaseo)
+ *  (c) 2013 Markus Blaschke (TEQneers GmbH & Co. KG) <blaschke@teqneers.de> (tq_seo)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+namespace Metaseo\Metaseo\Controller\Ajax\PageSeo;
+
+use Metaseo\Metaseo\Exception\Ajax\AjaxException;
+use Metaseo\Metaseo\Controller\Ajax\AbstractPageSeoController;
+use Metaseo\Metaseo\Controller\Ajax\PageSeoSimulateInterface;
+use Metaseo\Metaseo\Utility\DatabaseUtility;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility as Typo3GeneralUtility;
+
+class PageTitleSimController extends AbstractPageSeoController implements PageSeoSimulateInterface
+{
+    const LIST_TYPE = 'pagetitlesim';
+
+    /**
+     * @inheritDoc
+     */
+    protected function initFieldList()
+    {
+        $this->fieldList = array(
+            'title',
+            'tx_metaseo_pagetitle',
+            'tx_metaseo_pagetitle_rel',
+            'tx_metaseo_pagetitle_prefix',
+            'tx_metaseo_pagetitle_suffix',
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getIndex(array $page, $depth, $sysLanguage)
+    {
+        $list = $this->index($page, $depth, $sysLanguage, $this->fieldList);
+
+        $uidList = array_keys($list);
+
+        if (!empty($uidList)) {
+            // Check which pages have templates (for caching and faster building)
+            $this->templatePidList = array();
+
+            $query   = 'SELECT pid
+                          FROM sys_template
+                         WHERE pid IN (' . implode(',', $uidList) . ')
+                           AND deleted = 0
+                           AND hidden = 0';
+            $pidList = DatabaseUtility::getCol($query);
+            foreach ($pidList as $pid) {
+                $this->templatePidList[$pid] = $pid;
+            }
+
+            // Build simulated title
+            foreach ($list as &$row) {
+                $row['title_simulated'] = $this->simulateTitle($row, $sysLanguage);
+            }
+        }
+
+        return $list;
+    }
+
+    /**
+     * Generate simulated page title
+     *
+     * @param   array   $page        Page
+     * @param   integer $sysLanguage System language
+     *
+     * @return  string
+     */
+    protected function simulateTitle(array $page, $sysLanguage)
+    {
+        $this->initTsfe($page, null, $page, null, $sysLanguage);
+
+        $pagetitle = $this->objectManager->get('Metaseo\\Metaseo\\Page\\Part\\PagetitlePart');
+        $ret       = $pagetitle->main($page['title']);
+
+        return $ret;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function simulateAction()
+    {
+        try {
+            $this->init();
+            $ret = $this->executeSimulate();
+        } catch (AjaxException $ajaxException) {
+            return $this->ajaxErrorHandler($ajaxException);
+        }
+
+        return $this->ajaxSuccess($ret);
+    }
+
+    /**
+     * @return array
+     */
+    protected function executeSimulate()
+    {
+        $pid = (int)$this->postVar['pid'];
+
+        if (empty($pid)) {
+
+            return $this->ajaxErrorTranslate(
+                'message.error.typo3_page_not_found',
+                '[0x4FBF3C08]',
+                self::HTTP_STATUS_BAD_REQUEST
+            );
+        }
+
+        $page = BackendUtility::getRecord('pages', $pid);
+
+        if (empty($page)) {
+
+            return $this->ajaxErrorTranslate(
+                'message.error.typo3_page_not_found',
+                '[0x4FBF3C09]',
+                self::HTTP_STATUS_BAD_REQUEST
+            );
+        }
+
+        // Load TYPO3 classes
+        $this->initTsfe($page, null, $page, null);
+
+        $pagetitle = Typo3GeneralUtility::makeInstance(
+            'Metaseo\\Metaseo\\Page\\Part\\PagetitlePart'
+        );
+
+        return array(
+            'title' => $pagetitle->main($page['title']),
+        );
+    }
+}

Classes/Controller/AbstractAjaxController.php
@@ -26,6 +26,7 @@
 
 namespace Metaseo\Metaseo\Controller;
 
+use Metaseo\Metaseo\Exception\Ajax\AjaxException;
 use Exception;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -34,6 +35,8 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  */
 abstract class AbstractAjaxController
 {
+    const HTTP_CONTENT_TYPE_JSON = 'Content-type: application/json;charset=UTF-8';
+
     /**
      * Json status indicators
      */
@@ -114,101 +117,20 @@ abstract class AbstractAjaxController
      */
     protected $formProtection;
 
+    /**
+     * to be used for unit tests ONLY!
+     *
+     * @var boolean
+     */
+    protected $returnAsArray;
+
     // ########################################################################
     // Methods
     // ########################################################################
 
-    /**
-     * Execute ajax call
-     */
-    public function main()
+    public function __construct()
     {
-        $ajaxMethodName = $this->extractAjaxMethodName();
-
-        $ret = $this->callAjaxMethod($ajaxMethodName);
-
-        // Output json data
-        header('Content-type: application/json;charset=UTF-8');
-        echo json_encode($ret);
-        exit;
-    }
-
-    /**
-     * @return string
-     */
-    protected function extractAjaxMethodName()
-    {
-        // Try to find method
-        $ajaxMethodName = '';
-        if (!empty($_GET['cmd'])) {
-            // GET-param
-            $ajaxMethodName = (string)$_GET['cmd'];
-
-            // security
-            $ajaxMethodName = strtolower(trim($ajaxMethodName));
-            $ajaxMethodName = preg_replace('[^a-z]', '', $ajaxMethodName);
-        }
-
-        return $ajaxMethodName;
-    }
-
-    /**
-     * @param string $ajaxMethodName
-     *
-     * @return array
-     */
-    protected function callAjaxMethod($ajaxMethodName)
-    {
-        //check if empty
-        if (empty($ajaxMethodName)) {
-
-            return $this->ajaxErrorTranslate(
-                'message.warning.ajax_method_name_not_exist.message',
-                '[0x4FBF3C00]',
-                self::HTTP_STATUS_BAD_REQUEST
-            );
-        }
-
-        $methodName = 'execute' . $ajaxMethodName;
-        $call       = array($this, $methodName);
-
-        //check if not callable
-        if (!is_callable($call)) {
-
-            return $this->ajaxErrorTranslate(
-                'message.warning.ajax_method_name_not_exist.message',
-                '[0x4FBF3C07]',
-                self::HTTP_STATUS_BAD_REQUEST
-            );
-        }
-
-        //init
-        $this->fetchParams();
-        $this->init();
-
-        if (!$this->checkSessionToken()) {
-
-            return $this->ajaxErrorTranslate(
-                'message.error.access_denied',
-                '[0x4FBF3C06]',
-                self::HTTP_STATUS_UNAUTHORIZED
-            );
-        }
-
-        // Call function
-        try {
-            $ajaxArray = $this->$methodName();
-        } catch (Exception $e) {
-            //todo: log exception?
-
-            return $this->ajaxErrorTranslate(
-                'message.error.internal_server_error',
-                '[0x4FBF3C07]',
-                self::HTTP_STATUS_INTERNAL_SERVER_ERROR
-            );
-        }
-
-        return $ajaxArray;
+        $this->returnAsArray = false;
     }
 
     /**
@@ -255,6 +177,8 @@ abstract class AbstractAjaxController
      */
     protected function init()
     {
+        $this->fetchParams();
+
         // Include ajax local lang
         $this->getLanguageService()->includeLLFile('EXT:metaseo/Resources/Private/Language/locallang.xlf');
 
@@ -264,31 +188,39 @@ abstract class AbstractAjaxController
 
         // Init form protection instance
         $this->formProtection = $this->objectManager->get('TYPO3\\CMS\\Core\\FormProtection\\BackendFormProtection');
+        $this->checkSessionToken();
     }
 
     /**
      * Check session token
      *
-     * @return    boolean
+     * @todo Assumed TYPO3's CSRF protection is safe, this function can be removed.
+     * @see ExtensionManagementUtility
+     *
+     * @return array
      */
     protected function checkSessionToken()
     {
+        $sessionToken = $this->sessionToken($this->getAjaxPrefix());
 
-        if (empty($this->postVar['sessionToken'])) {
-            // No session token exists
-            return false;
+        if (empty($this->postVar['sessionToken']) // No session token exists
+            || $this->postVar['sessionToken'] != $sessionToken //session token is wrong
+        ) {
+
+            return $this->ajaxErrorTranslate(
+                'message.error.access_denied',
+                '[0x4FBF3C06]',
+                self::HTTP_STATUS_UNAUTHORIZED
+            );
         }
 
-        $className = strtolower(str_replace('\\', '_', get_class($this)));
-
-        $sessionToken = $this->sessionToken($className);
-
-        if ($this->postVar['sessionToken'] === $sessionToken) {
-            return true;
-        }
-
-        return false;
+        return array();
     }
+
+    /**
+     * @return string
+     */
+    abstract protected function getAjaxPrefix();
 
     /**
      * Create session token
@@ -324,10 +256,10 @@ abstract class AbstractAjaxController
     /**
      * Check if field is in table (TCA)
      *
-     * @param   string $table Table
-     * @param   string $field Field
+     * @param string $table Table
+     * @param string $field Field
      *
-     * @return  boolean
+     * @return boolean
      */
     protected function isFieldInTcaTable($table, $field)
     {
@@ -339,7 +271,9 @@ abstract class AbstractAjaxController
      * @param string $errorNumber
      * @param int    $httpStatus
      *
-     * @return array
+     * @return array ... this will never happen. Just keeps the IDE calm when using it in return statements
+     *
+     * @throws AjaxException
      */
     protected function ajaxErrorTranslate($messageKey = '', $errorNumber = '', $httpStatus = 400)
     {
@@ -355,29 +289,72 @@ abstract class AbstractAjaxController
      * @param string $errorNumber
      * @param int    $httpStatus
      *
-     * @return array
+     * @return array ... this will never happen. Just keeps the IDE calm when using it in return statements
+     *
+     * @throws AjaxException
      */
     protected function ajaxError($errorMessage = '', $errorNumber = '', $httpStatus = 400)
     {
         $httpStatus = (int)$httpStatus;
+
+        throw new AjaxException($errorMessage, $errorNumber, $httpStatus);
+    }
+
+
+    /**
+     * @param AjaxException $ajaxException
+     *
+     * @return array
+     *
+     * @throws AjaxException
+     */
+    protected function ajaxErrorHandler(AjaxException $ajaxException)
+    {
+        $httpStatus = $ajaxException->getHttpStatus();
         header('HTTP/1.0 ' . $httpStatus . ' ' . $this->httpStatus[$httpStatus]);
 
         $responseArray = array(
-            self::JSON_ERROR => $errorMessage
+            self::JSON_ERROR => $ajaxException->getMessage()
         );
 
-        if (!empty($errorNumber)) {
-            $responseArray[self::JSON_ERROR_NUMBER] = $errorNumber;
+        $errorCode = (string) $ajaxException->getCode();
+        if (!empty($errorCode)) {
+            $responseArray[self::JSON_ERROR_NUMBER] = $ajaxException->getCode();
         }
 
-        return $responseArray;
+        if ($this->returnAsArray) {
+            //to be used for unit tests ONLY!
+            throw $ajaxException;
+        }
+
+        return $this->renderExit($responseArray);
     }
 
+    /**
+     * @param array $data
+     *
+     * @return array
+     */
     protected function ajaxSuccess(array $data = array())
     {
         $data[self::JSON_SUCCESS] = true;
 
-        return $data;
+        return $this->renderExit($data);
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return array
+     */
+    protected function renderExit(array $data)
+    {
+        if ($this->returnAsArray === true) {
+            return $data;
+        }
+        header(self::HTTP_CONTENT_TYPE_JSON);
+        echo json_encode($data);
+        exit;
     }
 
     /**
@@ -410,5 +387,19 @@ abstract class AbstractAjaxController
     protected function getBackendUserAuthentication()
     {
         return $GLOBALS['BE_USER'];
+    }
+
+    /**
+     * to be used for unit tests ONLY!
+     *
+     * @param boolean $returnAsArray
+     *
+     * @return $this
+     */
+    public function setReturnAsArray($returnAsArray = true)
+    {
+        $this->returnAsArray = $returnAsArray;
+
+        return $this;
     }
 }

Classes/Controller/Ajax/PageSeo/AdvancedController.php
@@ -1,0 +1,181 @@
+<?php
+
+/*
+ *  Copyright notice
+ *
+ *  (c) 2015 Markus Blaschke <typo3@markus-blaschke.de> (metaseo)
+ *  (c) 2013 Markus Blaschke (TEQneers GmbH & Co. KG) <blaschke@teqneers.de> (tq_seo)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+namespace Metaseo\Metaseo\Controller\Ajax\PageSeo;
+
+use Metaseo\Metaseo\Controller\Ajax\AbstractPageSeoController;
+use Metaseo\Metaseo\Controller\Ajax\PageSeoInterface;
+use Metaseo\Metaseo\Utility\DatabaseUtility;
+
+class AdvancedController extends AbstractPageSeoController implements PageSeoInterface
+{
+    const LIST_TYPE = 'advanced';
+
+    /**
+     * @inheritDoc
+     */
+    protected function initFieldList()
+    {
+        $this->fieldList = array();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function executeIndex()
+    {
+        if (empty($this->postVar['pid'])) {
+
+            return $this->ajaxErrorTranslate(
+                'message.error.typo3_page_not_found',
+                '[0x4FBF3C0E]',
+                self::HTTP_STATUS_BAD_REQUEST
+            );
+        }
+
+        $ret = array();
+
+        $pid         = (int)$this->postVar['pid'];
+        $sysLanguage = (int)$this->postVar['sysLanguage'];
+
+
+        // check uid of pages language overlay
+        $query   = 'SELECT tag_name,
+                           tag_value
+                      FROM tx_metaseo_metatag
+                     WHERE pid = ' . (int)$pid . '
+                       AND sys_language_uid = ' . (int)$sysLanguage;
+        $rowList = DatabaseUtility::getAll($query);
+        foreach ($rowList as $row) {
+            $ret[$row['tag_name']] = $row['tag_value'];
+        }
+
+        return array(
+            'results' => count($ret),
+            'rows'    => array_values($ret),
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function executeUpdate()
+    {
+        if (empty($this->postVar['pid']) || empty($this->postVar['metaTags'])) {
+
+            return $this->ajaxErrorTranslate(
+                'message.error.typo3_page_not_found',
+                '[0x4FBF3C0F]',
+                self::HTTP_STATUS_BAD_REQUEST
+            );
+        }
+
+        $pid         = (int)$this->postVar['pid'];
+        $metaTagList = (array)$this->postVar['metaTags'];
+        $sysLanguage = (int)$this->postVar['sysLanguage'];
+
+
+        $this->clearMetaTags($pid, $sysLanguage);
+        $metaTagGroup = 2;
+        foreach ($metaTagList as $metaTagName => $metaTagValue) {
+            if (is_scalar($metaTagValue)) {
+                $metaTagValue = trim($metaTagValue);
+
+                if (strlen($metaTagValue) > 0) {
+                    $this->updateMetaTag($pid, $sysLanguage, $metaTagName, $metaTagValue);
+                }
+            } elseif (is_array($metaTagValue)) {
+                foreach ($metaTagValue as $subTagName => $subTagValue) {
+                    $this->updateMetaTag(
+                        $pid,
+                        $sysLanguage,
+                        array($metaTagName, $subTagName),
+                        $subTagValue,
+                        $metaTagGroup++
+                    );
+                }
+            }
+        }
+
+        return array();
+    }
+
+    /**
+     * Clear all meta tags for one page
+     *
+     * @param integer      $pid         PID
+     * @param integer|null $sysLanguage system language id
+     */
+    protected function clearMetaTags($pid, $sysLanguage)
+    {
+        $query = 'DELETE FROM tx_metaseo_metatag
+                        WHERE pid = ' . (int)$pid . '
+                          AND sys_language_uid = ' . (int)$sysLanguage;
+        DatabaseUtility::exec($query);
+    }
+
+    /**
+     * @param integer      $pid         PID
+     * @param integer|NULL $sysLanguage System language id
+     * @param string|array $metaTag     MetaTag name
+     * @param string       $value       MetaTag value
+     * @param integer      $tagGroup    MetaTag group
+     */
+    protected function updateMetaTag($pid, $sysLanguage, $metaTag, $value, $tagGroup = null)
+    {
+        $tstamp   = time();
+        $crdate   = time();
+        $cruserId = $this->getBackendUserAuthentication()->user['uid'];
+
+        $subTagName = '';
+
+        if (is_array($metaTag)) {
+            list($metaTag, $subTagName) = $metaTag;
+        }
+
+        if ($tagGroup === null) {
+            $tagGroup = 1;
+        }
+
+        $query = 'INSERT INTO tx_metaseo_metatag
+                              (pid, tstamp, crdate, cruser_id, sys_language_uid,
+                                  tag_name, tag_subname, tag_value, tag_group)
+                       VALUES (
+                             ' . (int)$pid . ',
+                             ' . (int)$tstamp . ',
+                             ' . (int)$crdate . ',
+                             ' . (int)$cruserId . ',
+                             ' . (int)$sysLanguage . ',
+                             ' . DatabaseUtility::quote($metaTag) . ',
+                             ' . DatabaseUtility::quote($subTagName) . ',
+                             ' . DatabaseUtility::quote($value) . ',
+                             ' . (int)$tagGroup . '
+                       ) ON DUPLICATE KEY UPDATE
+                               tstamp    = VALUES(tstamp),
+                               tag_value = VALUES(tag_value)';
+        DatabaseUtility::execInsert($query);
+    }
+}

Classes/Controller/Ajax/PageSeo/GeoController.php
@@ -1,0 +1,48 @@
+<?php
+
+/*
+ *  Copyright notice
+ *
+ *  (c) 2015 Markus Blaschke <typo3@markus-blaschke.de> (metaseo)
+ *  (c) 2013 Markus Blaschke (TEQneers GmbH & Co. KG) <blaschke@teqneers.de> (tq_seo)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+namespace Metaseo\Metaseo\Controller\Ajax\PageSeo;
+
+use Metaseo\Metaseo\Controller\Ajax\AbstractPageSeoController;
+use Metaseo\Metaseo\Controller\Ajax\PageSeoInterface;
+
+class GeoController extends AbstractPageSeoController implements PageSeoInterface
+{
+    const LIST_TYPE = 'geo';
+
+    /**
+     * @inheritDoc
+     */
+    protected function initFieldList()
+    {
+        $this->fieldList = array(
+            'tx_metaseo_geo_lat',
+            'tx_metaseo_geo_long',
+            'tx_metaseo_geo_place',
+            'tx_metaseo_geo_region'
+        );
+    }
+}

commit d27cdd60c3453f339ca2c59325562c81a348295b

[BUGFIX] Move simulate action from pagetitlesim to pagetitle
 This correction never affected a production version of MetaSeo
 (regression  was in develop branch only, introduced
 with a78f7304)

Issue #160

Classes/Controller/Ajax/PageSeo/PageTitleController.php
@@ -26,10 +26,14 @@
 
 namespace Metaseo\Metaseo\Controller\Ajax\PageSeo;
 
-use Metaseo\Metaseo\Controller\Ajax\AbstractPageSeoController;
-use Metaseo\Metaseo\Controller\Ajax\PageSeoInterface;
+use Metaseo\Metaseo\Controller\Ajax\AbstractPageSeoSimController;
+use Metaseo\Metaseo\Controller\Ajax\PageSeoSimulateInterface;
+use Metaseo\Metaseo\DependencyInjection\Utility\HttpUtility;
+use Metaseo\Metaseo\Exception\Ajax\AjaxException;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility as Typo3GeneralUtility;
 
-class PageTitleController extends AbstractPageSeoController implements PageSeoInterface
+class PageTitleController extends AbstractPageSeoSimController implements PageSeoSimulateInterface
 {
     const LIST_TYPE = 'pagetitle';
 
@@ -43,6 +47,45 @@ class PageTitleController extends AbstractPageSeoController implements PageSeoIn
             'tx_metaseo_pagetitle_rel',
             'tx_metaseo_pagetitle_prefix',
             'tx_metaseo_pagetitle_suffix',
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function executeSimulate()
+    {
+        $pid = (int)$this->postVar['pid'];
+
+        if (empty($pid)) {
+
+            throw new AjaxException(
+                'message.error.typo3_page_not_found',
+                '[0x4FBF3C08]',
+                HttpUtility::HTTP_STATUS_BAD_REQUEST
+            );
+        }
+
+        $page = BackendUtility::getRecord('pages', $pid);
+
+        if (empty($page)) {
+
+            throw new AjaxException(
+                'message.error.typo3_page_not_found',
+                '[0x4FBF3C09]',
+                HttpUtility::HTTP_STATUS_BAD_REQUEST
+            );
+        }
+
+        // Load TYPO3 classes
+        $this->getFrontendUtility()->initTsfe($page, null, $page, null);
+
+        $pagetitle = Typo3GeneralUtility::makeInstance(
+            'Metaseo\\Metaseo\\Page\\Part\\PagetitlePart'
+        );
+
+        return array(
+            'title' => $pagetitle->main($page['title']),
         );
     }
 }

Classes/Controller/Ajax/PageSeo/PageTitleSimController.php
@@ -26,14 +26,10 @@
 
 namespace Metaseo\Metaseo\Controller\Ajax\PageSeo;
 
-use Metaseo\Metaseo\Controller\Ajax\AbstractPageSeoSimController;
-use Metaseo\Metaseo\Controller\Ajax\PageSeoSimulateInterface;
-use Metaseo\Metaseo\DependencyInjection\Utility\HttpUtility;
-use Metaseo\Metaseo\Exception\Ajax\AjaxException;
-use TYPO3\CMS\Backend\Utility\BackendUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility as Typo3GeneralUtility;
+use Metaseo\Metaseo\Controller\Ajax\AbstractPageSeoController;
+use Metaseo\Metaseo\Controller\Ajax\PageSeoInterface;
 
-class PageTitleSimController extends AbstractPageSeoSimController implements PageSeoSimulateInterface
+class PageTitleSimController extends AbstractPageSeoController implements PageSeoInterface
 {
     const LIST_TYPE = 'pagetitlesim';
 
@@ -94,45 +90,6 @@ class PageTitleSimController extends AbstractPageSeoSimController implements Pag
         $ret       = $pagetitle->main($page['title']);
 
         return $ret;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    protected function executeSimulate()
-    {
-        $pid = (int)$this->postVar['pid'];
-
-        if (empty($pid)) {
-
-            throw new AjaxException(
-                'message.error.typo3_page_not_found',
-                '[0x4FBF3C08]',
-                HttpUtility::HTTP_STATUS_BAD_REQUEST
-            );
-        }
-
-        $page = BackendUtility::getRecord('pages', $pid);
-
-        if (empty($page)) {
-
-            throw new AjaxException(
-                'message.error.typo3_page_not_found',
-                '[0x4FBF3C09]',
-                HttpUtility::HTTP_STATUS_BAD_REQUEST
-            );
-        }
-
-        // Load TYPO3 classes
-        $this->getFrontendUtility()->initTsfe($page, null, $page, null);
-
-        $pagetitle = Typo3GeneralUtility::makeInstance(
-            'Metaseo\\Metaseo\\Page\\Part\\PagetitlePart'
-        );
-
-        return array(
-            'title' => $pagetitle->main($page['title']),
-        );
     }
 
     /**

Classes/DependencyInjection/Utility/FrontendUtility.php
@@ -89,8 +89,15 @@ class FrontendUtility implements SingletonInterface
         MetaseoFrontendUtility::init($page['uid'], $rootLine, $pageData, $rootlineFull, $sysLanguage);
     }
 
+    /**
+     * @param PageRepository $pageRepository
+     *
+     * @return $this
+     */
     public function setPageRepository(PageRepository $pageRepository)
     {
         $this->pageRepository = $pageRepository;
+
+        return $this;
     }
 }

Tests/Unit/Controller/Ajax/PageSeo/PageTitleControllerTest.php
@@ -26,10 +26,10 @@
 
 namespace Metaseo\Metaseo\Tests\Unit\Controller\Ajax\PageSeo;
 
-use Metaseo\Metaseo\Tests\Unit\Controller\Ajax\AbstractPageSeoControllerTest;
+use Metaseo\Metaseo\Tests\Unit\Controller\Ajax\AbstractPageSeoSimControllerTest;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class PageTitleControllerTest extends AbstractPageSeoControllerTest
+class PageTitleControllerTest extends AbstractPageSeoSimControllerTest
 {
     /**
      * @return \Metaseo\Metaseo\Controller\Ajax\PageSeo\PageTitleController

Tests/Unit/Controller/Ajax/PageSeo/PageTitleSimControllerTest.php
@@ -26,10 +26,10 @@
 
 namespace Metaseo\Metaseo\Tests\Unit\Controller\Ajax\PageSeo;
 
-use Metaseo\Metaseo\Tests\Unit\Controller\Ajax\AbstractPageSeoSimControllerTest;
+use Metaseo\Metaseo\Tests\Unit\Controller\Ajax\AbstractPageSeoControllerTest;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class PageTitleSimControllerTest extends AbstractPageSeoSimControllerTest
+class PageTitleSimControllerTest extends AbstractPageSeoControllerTest
 {
     /**
      * @return \Metaseo\Metaseo\Controller\Ajax\PageSeo\PageTitleSimController