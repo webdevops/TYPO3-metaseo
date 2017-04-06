15317d3f382f2d590ae3c48b52e43ae9c844513b
Simon Schaufelberger <simonschaufi@googlemail.com>
2017-04-06 15:14:47 +0200

    [BUGFIX] Use FontAwesome image as main module icon
    Fixes #359

--- ext_tables.php
+++ ext_tables.php
@@ -33,28 +33,41 @@ if (TYPO3_MODE == 'BE') {
         'web',
         'pageseo',
         '', # Position
-        array('BackendPageSeo' => 'main,metadata,geo,searchengines,url,pagetitle,pagetitlesim'), # Controller array
-        array(
+        ['BackendPageSeo' => 'main,metadata,geo,searchengines,url,pagetitle,pagetitlesim'], # Controller array
+        [
             'access' => 'user,group',
             'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Backend/Icons/ModuleSeo.png',
             'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/ModuleSeo/locallang.xlf',
-        )
+        ]
     );
 
     // ####################################################
     // Module category "SEO"
     // ####################################################
 
+	/** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
+	$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+		\TYPO3\CMS\Core\Imaging\IconRegistry::class
+	);
+	$iconRegistry->registerIcon(
+		'module-seo',
+		\TYPO3\CMS\Core\Imaging\IconProvider\FontawesomeIconProvider::class,
+		[
+			'name' => 'bullseye'
+		]
+	);
+
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
         $_EXTKEY,
         'metaseo',
         '',
         '',
-        array(),
-        array(
+        [],
+        [
             'access' => 'user,group',
+            'iconIdentifier' => 'module-seo',
             'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/ModuleMain/locallang.xlf',
-        )
+        ]
     );
 
 
@@ -63,12 +76,12 @@ if (TYPO3_MODE == 'BE') {
         'metaseo',
         'controlcenter',
         '',
-        array('BackendControlCenter' => 'main'),
-        array(
+        ['BackendControlCenter' => 'main'],
+        [
             'access' => 'user,group',
             'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Backend/Icons/ModuleControlCenter.png',
             'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/ModuleControlCenter/locallang.xlf',
-        )
+        ]
     );
 
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
@@ -76,12 +89,12 @@ if (TYPO3_MODE == 'BE') {
         'metaseo',
         'sitemap',
         'after:controlcenter',
-        array('BackendSitemap' => 'main,sitemap'),
-        array(
+        ['BackendSitemap' => 'main,sitemap'],
+        [
             'access' => 'user,group',
             'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Backend/Icons/ModuleSitemap.png',
             'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/ModuleSitemap/locallang.xlf',
-        )
+        ]
     );
 
     // ############################################################################
