22ebf0e31bcf6a42af84e4041944bd81a0f0d614
Thomas Mayer <thomas.mayer@2bis10.de>
2017-04-19 09:08:50 +0200

    Revert "[FEATURE] 8LTS/TCA: Don't touch TCA from ext_tables.php"
    This reverts commit fc6998dabcd4f08b2a1cbbb1cb1e077d131bc918.

--- ext_tables.php
+++ ext_tables.php
@@ -2,6 +2,23 @@
 defined('TYPO3_MODE') or exit;
 
 // ############################################################################
+// TABLES
+// ############################################################################
+
+// ################
+// Settings Root
+// ################
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_metaseo_setting_root',
+    'EXT:metaseo/Resources/Private/Language/locallang.tca.xml'
+);
+
+// allow pages which contain such a record to be copied by users without throwing errors
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_metaseo_setting_root');
+
+
+// ############################################################################
 // Backend
 // ############################################################################
 
@@ -93,3 +110,8 @@ if (TYPO3_MODE == 'BE') {
     );
 }
 
+// ############################################################################
+// CONFIGURATION
+// ############################################################################
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'MetaSEO');
